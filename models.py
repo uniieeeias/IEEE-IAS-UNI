4c4f543091b7112e04bad1d50b19705ee335923c
uniieeeias <uniieeeias@gmail.com>
2026-04-21 18:32:48 -0500

    Update models.py

--- models.py
+++ models.py
@@ -2,13 +2,21 @@ from sqlalchemy import Column, Integer, String, DateTime
 from database import Base
 from datetime import datetime
 
+
 class Certificate(Base):
     __tablename__ = "certificates"
 
     id = Column(Integer, primary_key=True, index=True)
     serial = Column(String, unique=True, index=True)
+
+    event_code = Column(String)
     event_name = Column(String)
     event_type = Column(String)
+
     participant = Column(String)
-    status = Column(String, default="valid")
+
+    status = Column(String, default="valid")  # valid / revoked / expired
+
+    issued_by = Column(String, default="IEEE IAS UNI")
+
     created_at = Column(DateTime, default=datetime.utcnow)
