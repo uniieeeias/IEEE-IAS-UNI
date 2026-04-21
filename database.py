2de743f75ca991b1d86459af1ae9a5128bbe2e50
uniieeeias <uniieeeias@gmail.com>
2026-04-21 17:36:42 -0500

    database.py

--- database.py
+++ database.py
@@ -1,10 +1,8 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "sqlite:///./certificates.db"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(DATABASE_URL)
 
-SessionLocal = sessionmaker(bind=engine)
-
-Base = declarative_base()
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
